a4c4f1fc79b9b9c24dcbae7dfff5ddb5d8c369f0
pzl <dan@panzarel.la>
2017-12-07 10:59:23 -0500

    initial cart setup

--- layouts/default.vue
+++ layouts/default.vue
@@ -2,19 +2,22 @@
   <div id="page">
   	<sidebar />
     <nuxt/>
+  	<cart />
   </div>
 </template>
 
 <style>
 #page {
 	display: flex;
+	justify-content: space-between;
 }
 </style>
 
 <script>
 import Sidebar from '~/components/Sidebar'
+import Cart from '~/components/Cart'
 
 export default {
-	components: { Sidebar }
+	components: { Sidebar, Cart }
 }
 </script>
---
+++ pages/donate.vue
@@ -0,0 +1,21 @@
+<template>
+	<main>
+		<div id="content">
+			<h1>Donate</h1>
+			<h2>Thank you for your interest in our cause</h2>
+
+			<p>Here are a few senators that support our cause, and would greatly benefit from a contribution on your behalf. Please select a few to donate to</p>
+
+
+
+		</div>
+	</main>
+</template>
+
+<style src="../assets/css/donate.css"></style>
+
+<script>
+export default {
+
+}
+</script>
